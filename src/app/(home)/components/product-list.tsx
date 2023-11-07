68ddda7517df6fd7a76bc9c2417e05b43c15415c
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-07 02:27:21 -0300

    feat: add price and discount to the product-item

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -1,4 +1,5 @@
 import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
 
 interface ProductListProps {
@@ -9,7 +10,7 @@ const ProductList = ({products}: ProductListProps) => {
     
     return(
         <div className="flex w-full gap-4 overflow-x-auto px-5 ">
-            {products.map(product => <ProductItem key={product.id} product={product}/>)}
+            {products.map(product => <ProductItem key={product.id} product={computeProductTotalPrice(product)}/>)}
         </div>
     );
 }
--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,13 +1,13 @@
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 
 interface ProductItemProps {
-    product: Product
+    product: ProductWithTotalPrice
 }
 
 const ProductItem = ({product}: ProductItemProps) => {
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 max-w-[156px]">
             {/* IMAGEM */}
             <div className="bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
                 {/* sempre que quiser utilizar medidas dinamicas, 
@@ -29,9 +29,31 @@ const ProductItem = ({product}: ProductItemProps) => {
                 />
             </div>
 
-            {/* TEXTO 1:04*/}
+            {/* TEXTO 1:16*/}
             <div>
+                <p className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+                    {product.name}
+                </p>
+            </div>
 
+            <div className="flex items-center gap-2">
+                {product.discountPercentage > 0 ? (
+                    <>
+                        <p className="font-semibold text-sm">
+                            R$ {product.totalPrice.toFixed(2)}
+                        </p>
+                        
+                        <p className="line-through opacity-75 text-xs">
+                            R$ {Number(product.basePrice).toFixed(2)}
+                        </p>
+                    </>
+                ) : (
+                    <p className="font-semibold text-sm">
+                        R$ {product.basePrice.toFixed(2)}
+                    </p>
+                )}
+                
+                
             </div>
         </div>
     )
