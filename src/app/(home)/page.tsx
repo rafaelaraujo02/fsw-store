4309e32cf1122b4cbfe6e40189efaa10a4c5352b
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-08 04:05:54 -0300

    feat: add second banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,19 @@ export default async function Home() {
       </div>
 
       <div className='mt-8'>
+        <p className='font-semibold uppercase mb-3 pl-5'>ofertas</p>
         <ProductList products={deals}/>
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        width={0}
+        height={0}
+        className='h-auto w-full px-5'
+        sizes='100vw'
+        alt='Até 55% de desconto em mouses!'
+      />
+
     </div>
   )
 }
