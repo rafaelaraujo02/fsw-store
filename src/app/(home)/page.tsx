98dbcc5ce45fdc9ba1cc7bb2395ab7c9a7d487bc
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-09 04:12:37 -0300

    feat: add keyboards section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Categories from './components/categories';
 import { prismaClient } from '@/lib/prisma';
 import ProductList from './components/product-list';
+import SectionTitle from './components/section-title';
+import PromoBanner from './components/promo-banner';
 
 export default async function Home() {
 
@@ -13,14 +15,18 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'keyboards'
+      }
+    }
+  });
+
   return (
     <div>
-      <Image
+      <PromoBanner
         src="/banner-home-01.png"
-        width={0}
-        height={0}
-        className='h-auto w-full px-5'
-        sizes='100vw'
         alt='Até 55% de desconto esse mês!'
       />
 
@@ -29,18 +35,19 @@ export default async function Home() {
       </div>
 
       <div className='mt-8'>
-        <p className='font-semibold uppercase mb-3 pl-5'>ofertas</p>
+      <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals}/>
       </div>
-
-      <Image
-        src="/banner-home-02.png"
-        width={0}
-        height={0}
-        className='h-auto w-full px-5'
-        sizes='100vw'
+    {/* 1:28 */}
+      <PromoBanner 
+        src="/banner-home-02.png" 
         alt='Até 55% de desconto em mouses!'
       />
+
+      <div className='mt-8'>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards}/>
+      </div>
 
     </div>
   )
