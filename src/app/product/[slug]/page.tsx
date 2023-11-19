ab14296f560d38012fcd90ab79ad6d195c6dd225
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-18 21:22:42 -0300

    fix: ensure component proportion

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { prismaClient } from "@/lib/prisma"
 import ProductImages from "./components/product-image"
+import ProductInfo from "./components/product-info"
+import { computeProductTotalPrice } from "@/helpers/product"
 
 interface ProductDetailPageProps {
     params: {
@@ -18,8 +20,9 @@ const ProductDetailPage = async ({params: { slug }}: ProductDetailPageProps) => 
     if(!product) return null;
     
     return(
-        <div>
+        <div className="flex flex-col gap-8">
             <ProductImages imageUrls={product.imageUrls} name={product.name}/>
+            <ProductInfo product={computeProductTotalPrice(product)}/>
         </div>
     )
 }
