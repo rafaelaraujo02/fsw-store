ad3e995892fee26b99bea4d0624540a5582d3083
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-29 23:45:13 -0300

    feat: add logic for an item that already exists, or not, in the CartItem

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends ProductWithTotalPrice {
     quantity: number;
 }
 
@@ -22,12 +22,31 @@ export const CartContext = createContext<ICartContext>({
     cartTotalDiscount: 0,
     addProductToCart: () => {},
 })
-
+//2:14
 const CartProvider = ({children}: {children: ReactNode}) => {
     
     const [products, setProducts] = useState<CartProduct[]>([])
 
     const addProductToCart = (product: CartProduct) => {
+
+        //se o produto já estiver no carrinho, só aumenta a quantidade
+        const productIsAlreadyOnCart = products.some(cartProduct => cartProduct.id === product.id)
+
+        if(productIsAlreadyOnCart){
+            setProducts(prev => prev.map(cartProduct => {
+                if(cartProduct.id === product.id){
+                    return{
+                        ...cartProduct,
+                        quantity: cartProduct.quantity + product.quantity
+                    }
+                }
+                return cartProduct
+                }),
+            );
+
+            return;
+        }
+        //senão, adicione o produto à lista
         setProducts((prev) => [...prev, product])
     }
 
