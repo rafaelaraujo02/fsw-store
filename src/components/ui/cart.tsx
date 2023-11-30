43159c256aabeea2f47df875bef7f79cc634e4e2
Rafael <rafaelaraujo.02@hotmail.com>
2023-11-29 23:34:16 -0300

    feat: add render logic to cart item

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,13 +2,15 @@ import { ShoppingCartIcon } from "lucide-react"
 import { Badge } from "./badge"
 import { CartContext } from "@/providers/cart"
 import { useContext } from "react";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
 
     const { products } = useContext(CartContext);
     
     return(
-        <div>
+        <div className="flex flex-col gap-5">
             <Badge 
                 className="w-fit gap-1 text-base uppercase border-primary px-3 py-[0.375rem] border-2"
                 variant='outline'
@@ -17,11 +19,13 @@ const Cart = () => {
                     Catálogo
             </Badge>
             {/* RENDERIZAR PRODUTOS */}
-            {products.map((product) => (
-                <h1 key={product.id}>
-                    {product.name}
-                </h1>
-            ))}
+            <div className="flex flex-col gap-5">
+                {products.map((product) => (
+                    <h1 key={product.id}>
+                        <CartItem key={product.id} product={computeProductTotalPrice(product as any) as any}/>
+                    </h1>
+                ))}
+            </div>
         </div>
     )
 }
